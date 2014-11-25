c38492e79b70179ca716c84a0225e0b8295871be
Franco Maria Nardini <francomaria.nardini@isti.cnr.it>
2014-11-25 11:46:26 +0100

    adding new unit tests

---
+++ unit-tests/learning/test-xml-load.cc
@@ -0,0 +1,12 @@
+#define BOOST_TEST_XML_LOAD
+#include <boost/test/unit_test.hpp>
+
+#include "learning/ltr_algorithm.h"
+
+#include <cmath>
+
+BOOST_AUTO_TEST_CASE( test_xml_load ) {
+
+  // read model
+  quickrank::learning::LTR_Algorithm* prova = quickrank::learning::LTR_Algorithm::load_model_from_file("tests/data/msn.fold1.quickrank.lmart.xml");
+}
