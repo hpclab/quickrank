d459a246898d8b65f0daf2068895c418a91301ac
claudio <claudio.lucchese@isti.cnr.it>
2014-12-10 07:49:35 +0100

    minor test product

--- unit-tests/io/test-xml-io.cc
+++ unit-tests/io/test-xml-io.cc
@@ -25,10 +25,10 @@ BOOST_AUTO_TEST_CASE( test_xml_io ) {
 
   quickrank::io::Xml xml;
 
-  xml.generate_c_code_baseline("tests/msn.fold1.quickrank.lmart.xml", "tests/lmart.cc");
+  xml.generate_c_code_baseline("tests/model.xml", "tests/lmart.cc");
 
   // xml.generate_c_code_oblivious_trees("tests/msn.fold1.quickrank.matrixnet.xml", "tests/matrixnet.cc");
 
-  xml.generate_c_code_vectorized("tests/msn.fold1.quickrank.lmart.xml", "tests/lmart-fast.cc");
+  xml.generate_c_code_vectorized("tests/model.xml", "tests/lmart-fast.cc");
 
 }
--- unit-tests/learning/test-product.cc
+++ unit-tests/learning/test-product.cc
@@ -27,23 +27,30 @@
 
 BOOST_AUTO_TEST_CASE( test_product ) {
 
-  float* scores = new float[5000]();
+  unsigned int cache_size = 3000000/sizeof(float);
+  unsigned int num_rounds = 10;
+  unsigned int num_docs = 10*cache_size; // this is more than 3MB
+  unsigned int num_fx = 136;
+
+  std::cout << "Num docs: " << num_docs << std::endl;
+
+  float* scores = new float[num_docs]();
 
   std::cout << "init data" << std::endl;
-  float* data = new float[5000 * 136];
-  for (unsigned int i = 0; i < 5000 * 136; i++)
+  float* data = new float[num_docs * num_fx];
+  for (unsigned int i = 0; i < num_docs * num_fx; i++)
     data[i] = static_cast<float>(rand())
         / (static_cast<float>(RAND_MAX / 10.0));
   std::cout << "init weights" << std::endl;
-  float* w = new float[136];
-  for (unsigned int i = 0; i < 136; i++)
+  float* w = new float[num_fx];
+  for (unsigned int i = 0; i < num_fx; i++)
     w[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 10.0));
 
   auto start_serial = std::chrono::high_resolution_clock::now();
-  for (unsigned int r = 0; r < 100; r++) {
-    for (unsigned int d = 0; d < 5000; d++) {
-      for (unsigned int f = 0; f < 136; f++) {
-        scores[d] += w[f]*data[d*136 + f];
+  for (unsigned int r = 0; r < num_rounds; r++) {
+    for (unsigned int d = 0; d < num_docs; d++) {
+      for (unsigned int f = 0; f < num_fx; f++) {
+        scores[d] += w[f]*data[d*num_fx + f];
       }
     }
   }
@@ -52,11 +59,11 @@ BOOST_AUTO_TEST_CASE( test_product ) {
       end_serial - start_serial).count();
 
   auto start_parallel = std::chrono::high_resolution_clock::now();
-  for (unsigned int r = 0; r < 100; r++) {
-#pragma omp parallel for num_threads(2)
-    for (unsigned int d = 0; d < 5000; d++) {
-      for (unsigned int f = 0; f < 136; f++) {
-        scores[d] += w[f]*data[d*136 + f];
+  for (unsigned int r = 0; r < num_rounds; r++) {
+#pragma omp parallel for //num_threads(2)
+    for (unsigned int d = 0; d < num_docs; d++) {
+      for (unsigned int f = 0; f < num_fx; f++) {
+        scores[d] += w[f]*data[d*num_fx + f];
       }
     }
   }
@@ -68,7 +75,7 @@ BOOST_AUTO_TEST_CASE( test_product ) {
   std::cout
       << "Random score: "
       << scores[static_cast<int>(static_cast<float>(rand())
-          / (static_cast<float>(RAND_MAX / 5000.0)))]
+          / (static_cast<float>(RAND_MAX) / num_docs))]
       << std::endl;
 
   std::cout << "Serial time: " << time_serial << std::endl;
