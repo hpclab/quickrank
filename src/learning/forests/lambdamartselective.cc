c151a93e50f29c76493b485c295a0cf818ae3dc5
Salvatore Trani <trani.salvatore@gmail.com>
2018-04-17 15:58:20 +0200

    bug fix

--- src/learning/forests/lambdamartselective.cc
+++ src/learning/forests/lambdamartselective.cc
@@ -385,13 +385,14 @@ size_t LambdaMartSelective::sampling_query_level(
     if (negative_strategy == "RATIO") {
       n_top_neg = (size_t) std::round(rank_factor * n_neg_query);
       n_random_neg = (size_t) std::round(random_factor * n_neg_query);
+
     } else if (negative_strategy == "MUL") {
       n_top_neg = (size_t) std::round(rank_factor * npositives[q]);
       n_random_neg = (size_t) std::round(random_factor * npositives[q]);
       n_top_neg = std::min(n_top_neg, n_neg_query);
       n_random_neg = std::min(n_random_neg, n_neg_query);
-    } else if (negative_strategy == "POS") {
 
+    } else if (negative_strategy == "POS") {
       if (npositives[q] == 0) {
 
         n_top_neg = 0;
@@ -405,19 +406,19 @@ size_t LambdaMartSelective::sampling_query_level(
                   });
 
         size_t last_pos = 0;
-        size_t num_pos_last = 0;
-        #pragma omp parallel for reduction(max : last_pos), reduction(+ : num_pos_last)
+        #pragma omp parallel for reduction(max : last_pos)
         for (size_t i=0; i<query_size; ++i) {
           if (dataset->getLabel(sampleids[start_offset + i]) > 0) {
             last_pos = i;
-            ++num_pos_last;
           }
         }
 
-  //      std::cout << "Position Last positive: " << last_pos << std::endl;
-        size_t n_neg_before_last_pos = last_pos - num_pos_last;
+        size_t n_neg_before_last_pos = last_pos - npositives[q] + 1;
         n_top_neg = (size_t) std::round(rank_factor * n_neg_before_last_pos);
         n_random_neg = (size_t) std::round(random_factor * n_neg_before_last_pos);
+//        std::cout << "Position Last positive: " << last_pos
+//                  << " - n_neg_before_last_pos: " << n_neg_before_last_pos
+//                  << std::endl;ok tro
       }
     } else {
       throw std::logic_error("Not supported!");
